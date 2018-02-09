eecd522530cb939e1e63a4408797d1fbddb1e316
Unknown <monika.silvia.s@hotmail.ca>
2018-02-08 22:50:28 -0800

    database added

---
+++ database/hangman.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2018 at 07:50 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hangman`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+CREATE TABLE `player` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `score` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `player`
+--
+
+INSERT INTO `player` (`id`, `name`, `score`) VALUES
+(61, 'nnnnnn', 11),
+(62, 'nnnnn', 11),
+(63, '11111', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `word_guessed`
+--
+
+CREATE TABLE `word_guessed` (
+  `id` int(11) NOT NULL,
+  `word` varchar(250) NOT NULL,
+  `definition` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `word_guessed`
+--
+
+INSERT INTO `word_guessed` (`id`, `word`, `definition`) VALUES
+(61, 'saorsa', 'the wall of a room or of a garden'),
+(62, 'alcoved', 'relating to electricity produced by chemical action in battery'),
+(63, 'elution', 'the wall of a room or of a garden');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `word_guessed`
+--
+ALTER TABLE `word_guessed`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `player`
+--
+ALTER TABLE `player`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `word_guessed`
+--
+ALTER TABLE `word_guessed`
+  ADD CONSTRAINT `word_guessed_ibfk_1` FOREIGN KEY (`id`) REFERENCES `player` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
